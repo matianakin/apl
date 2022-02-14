c01a10b21990789a4d3cafafb7a964c8469a093a
mategra599 <mategra599@student.polsl.pl>
2022-02-14 22:39:35 +0100

    release

--- APLDijkstra/Dijkstra/Dijkstra/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ APLDijkstra/Dijkstra/Dijkstra/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -157,7 +157,7 @@ namespace Dijkstra {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Dijkstra;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Dijkstra;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
